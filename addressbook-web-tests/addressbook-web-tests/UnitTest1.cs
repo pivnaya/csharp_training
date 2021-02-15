dc1f2ff874836bdc6932b92a4dd9a7a883157563
Aleksandra Pivovarova <yourfreckle@mail.ru>
2021-02-15 23:38:09 +1000

    Задание 3. Вспомогательные методы и классы

--- addressbook-web-tests/addressbook-web-tests/UnitTest1.cs
+++ addressbook-web-tests/addressbook-web-tests/UnitTest1.cs
@@ -7,8 +7,39 @@ namespace addressbook_web_tests
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestMethodSquare()
         {
+            Square s1 = new Square(5);
+            Square s2 = new Square(10);
+            Square s3 = s1;
+
+            Assert.AreEqual(5, s1.Size);
+            Assert.AreEqual(10, s2.Size);
+            Assert.AreEqual(5, s3.Size);
+
+            s3.Size = 15;
+
+            Assert.AreEqual(15, s1.Size);
+
+            s2.Colored = true;
+        }
+
+        [TestMethod]
+        public void TestMethodCircle()
+        {
+            Circle s1 = new Circle(5);
+            Circle s2 = new Circle(10);
+            Circle s3 = s1;
+
+            Assert.AreEqual(5, s1.Radius);
+            Assert.AreEqual(10, s2.Radius);
+            Assert.AreEqual(5, s3.Radius);
+
+            s3.Radius = 15;
+
+            Assert.AreEqual(15, s1.Radius);
+
+            s2.Colored = true;
         }
     }
 }
