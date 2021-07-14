8817b74cccfc8e0828a6894a0c60e3d3acd0d0df
Aleksandra Pivovarova <yourfreckle@mail.ru>
2021-07-14 23:54:39 +1000

    BDD сценарии + правка readme

---
+++ addressbook-web-tests/addressbook-web-tests/bdd/Login.feature
@@ -0,0 +1,11 @@
+﻿Feature: Login
+
+Scenario: Login with valid credentials
+	Given A user is logged out
+	When I login with username "admin" and password "secret"
+	Then I have logged in
+
+Scenario: Login with invalid credentials
+	Given A user is logged out
+	When I login with username "user" and password "secret"
+	Then I have not logged in
---
+++ addressbook-web-tests/addressbook-web-tests/bdd/LoginSteps.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using TechTalk.SpecFlow;
+using WebAddressbookTests;
+
+namespace addressbook_web_tests.bdd
+{
+    [Binding]
+    public class LoginSteps
+    {
+        private ApplicationManager app
+        {
+            get { return ApplicationManager.GetInstance(); }
+        }
+
+        [Given(@"A user is logged out")]
+        public void GivenAUserIsLoggedOut()
+        {
+            app.Auth.Logout();
+        }
+
+        [When(@"I login with username ""(.*)"" and password ""(.*)""")]
+        public void WhenILoginWithValidCredentials(string username, string password)
+        {
+            AccountData account = new AccountData(username, password);
+            ScenarioContext.Current.Add("account", account);
+            app.Auth.Login(account);
+        }
+
+        [Then(@"I have logged in")]
+        public void ThenIHaveLoggedIn()
+        {
+            AccountData account = ScenarioContext.Current.Get<AccountData>("account");
+            Assert.IsTrue(app.Auth.IsLoggedIn(account));
+        }
+
+        [Then(@"I have not logged in")]
+        public void ThenIHaveNotLoggedIn()
+        {
+            AccountData account = ScenarioContext.Current.Get<AccountData>("account");
+            Assert.IsFalse(app.Auth.IsLoggedIn(account));
+        }
+    }
+}
